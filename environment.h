f203d94d1ef724f6d14bc89d3f27543c919ba686
khalifa47 <khalifafumo5@gmail.com>
2022-11-19 11:37:53 +0300

    changed environment

--- environment.h
+++ environment.h
@@ -3,8 +3,8 @@ void drawEnvironment()
 {
 	glPushMatrix();
 
-	glColor3f(0.0, 0.48, 0.047);		// green floor
-
+	// green floor
+	glColor3f(0.0, 0.48, 0.047);
 	glBegin(GL_POLYGON);
 	glVertex3f(0.0, 9.0, 0.0);
 	glVertex3f(100.0, 9.0, 0.0);
@@ -14,7 +14,8 @@ void drawEnvironment()
 	glVertex3f(0.0, 9.0, 0.0);
 	glEnd();
 
-	glColor3f(0.474, 0.298, 0.074);  // brown ground
+	// brown ground
+	glColor3f(0.474, 0.298, 0.074);
 	glBegin(GL_POLYGON);
 	glVertex3f(0.0, 0.0, 0.0);
 	glVertex3f(100.0, 0.0, 0.0);
@@ -23,20 +24,12 @@ void drawEnvironment()
 	glVertex3f(0.0, 9.0, 0.0);
 	glEnd();
 
+	// blue sky
 	glColor3f(0.5, 0.6, 0.79);
-	glBegin(GL_POLYGON);				//ceiling
+	glBegin(GL_POLYGON);
 	glVertex3f(0.0, 100.0, 0.0);
 	glVertex3f(100.0, 100.0, 0.0);
-	glColor3f(0.6, 0.7, 0.89);
-	glVertex3f(100.0, 80.0, 0.0);
-	glVertex3f(0.0, 80.0, 0.0);
-	glEnd();
-
-	glColor3f(0.5, 0.6, 0.79);// sky blue
-	glBegin(GL_POLYGON);   //background screen
-	glVertex3f(0.0, 90.0, 5.0);
-	glVertex3f(100.0, 90.0, 5.0);
-	glColor3f(0.7, 0.8, 0.99);//sky
+	glColor3f(0.7, 0.8, 0.99);
 	glVertex3f(100.0, 10.0, 5.0);
 	glVertex3f(0.0, 10.0, 5.0);
 	glEnd();
